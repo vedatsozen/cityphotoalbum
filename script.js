688509d9ba9e84309ee32d5da30ae445041799e6
Vedat Sözen <vedatsozen@gmail.com>
2022-11-26 01:32:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+
+document.getElementById("photoid").src = "images/kizkulesi.jpg" ;
+
+function kizkulesi() {
+
+document.getElementById("photoid").src = "images/kizkulesi.jpg" ;
+
+}
+
+function camlicakulesi() {
+
+    document.getElementById("photoid").src = "images/camlicakulesi.jpg" ;
+    
+    }
+
+    function pierreloti() {
+
+        document.getElementById("photoid").src = "images/pierreloti.jfif" ;
+        
+        }
+
+        /* ANKARA */
+
+        
+function anitkabir() {
+
+    document.getElementById("photoid").src = "images/anitkabir.jpg" ;
+    
+    }
+    
+    function golbasi() {
+    
+        document.getElementById("photoid").src = "images/golbasi.jpg" ;
+        
+        }
+    
+        function kizilay() {
+    
+            document.getElementById("photoid").src = "images/kizilay.jpg" ;
+            
+            }
+
+            /* İZMİR */
+
+            function seferi() {
+
+                document.getElementById("photoid").src = "images/seferi.jfif" ;
+                
+                }
+                
+                function urla() {
+                
+                    document.getElementById("photoid").src = "images/urla.jpg" ;
+                    
+                    }
+                
+                    function konak() {
+                
+                        document.getElementById("photoid").src = "images/konak.jfif" ;
+                        
+                        }
+            
+
+        
+    function istanbul() {
+        document.getElementById("istanbul").style.display = "block";
+        document.getElementById("ankara").style.display = "none";
+        document.getElementById("izmir").style.display = "none";
+
+    }
+
+    function izmir() {
+        document.getElementById("izmir").style.display = "block";
+        document.getElementById("ankara").style.display = "none";
+        document.getElementById("istanbul").style.display = "none";
+
+    }
+
+    
+    function ankara() {
+        document.getElementById("ankara").style.display = "block";
+        document.getElementById("izmir").style.display = "none";
+        document.getElementById("istanbul").style.display = "none";
+
+    }
